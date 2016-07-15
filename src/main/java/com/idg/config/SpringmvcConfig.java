46c5137a3106f8610d5690eb89466b8754a7454a
叶浩 <77729052@qq.com>
2016-07-15 15:11:28 +0800

    添加springmvc注解配置，视图解析和访问静态资源配置

--- src/main/java/com/idg/config/SpringmvcConfig.java
+++ src/main/java/com/idg/config/SpringmvcConfig.java
@@ -1,7 +1,13 @@
 package com.idg.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
  * Created by yehao on 16/7/15.
@@ -9,8 +15,29 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  */
 @Configuration
 @EnableWebMvc
-public class SpringmvcConfig {
+@ComponentScan(basePackages = {"com.idg.web"})
+public class SpringmvcConfig extends WebMvcConfigurerAdapter {
 
-    
+    /**
+     * 配置视图解析器
+     *
+     * @return
+     */
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/WEB-INFO/view/");
+        internalResourceViewResolver.setSuffix(".jsp");
+        return internalResourceViewResolver;
+    }
 
+    /**
+     * 配置静态资源处理
+     *
+     * @param configurer
+     */
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
 }
