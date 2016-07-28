c86f2b59bcb4f1ae5f574c16e77ce79596b1fcd3
叶浩 <77729052@qq.com>
2016-07-28 16:00:43 +0800

    编码过滤器

--- src/main/java/config/ServletInitializer.java
+++ src/main/java/config/ServletInitializer.java
@@ -2,6 +2,7 @@ package config;
 
 import com.idg.common.filter.DemoFilter;
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
@@ -14,6 +15,10 @@ public class ServletInitializer implements WebApplicationInitializer {
 
     public void onStartup(ServletContext servletContext) throws ServletException {
         FilterRegistration.Dynamic filter = servletContext.addFilter("DemoFilter", DemoFilter.class);
-        filter.addMappingForUrlPatterns(null,false,"/*");
+        filter.addMappingForUrlPatterns(null, false, "/*");
+        FilterRegistration.Dynamic encodingFilter = servletContext.addFilter("encodingFilter", CharacterEncodingFilter.class);
+        filter.setInitParameter("encoding", "utf-8");
+        filter.setInitParameter("forceEncoding", "true");
+        filter.addMappingForUrlPatterns(null, false, "/*");
     }
 }
